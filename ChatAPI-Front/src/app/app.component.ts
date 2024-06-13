96250dc853f5188854084008b93cb067b634bf48
levimaciell <levimaciell1221@gmail.com>
2024-06-13 11:00:28 -0300

    basic login component

--- ChatAPI-Front/src/app/app.component.ts
+++ ChatAPI-Front/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    LoginComponent
+  ],
+  template: `
+  <app-login/>
+  <router-outlet />
+  `
 })
 export class AppComponent {
   title = 'ChatAPI-Front';
---
+++ ChatAPI-Front/src/app/modules/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+    public username = "";
+    public password = "";
+
+  login(){
+    let json = {
+      "username": this.username,
+      "passord": this.password
+    }
+    console.log(json);
+  }
+}
