6604c6f07491e6bbeb68dba561ac27ee33e59155
levimaciell <levimaciell1221@gmail.com>
2024-06-14 12:50:05 -0300

    Improve directory structure and adding a login page to home component

--- ChatAPI-Front/src/app/app.component.ts
+++ ChatAPI-Front/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './modules/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    LoginComponent
+    RouterOutlet 
   ],
   template: `
-  <app-login/>
   <router-outlet />
   `
 })
